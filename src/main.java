479e4030bea42e8a838639d90a25c25b4a831bd5
Kimmidog <Kimmidog4@gmail.com>
2024-09-24 19:23:22 -0400

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,17 @@
+public class main {
+    public static void main(String[] args){
+        double springCost = 550.00;
+        double summerCost = 650.00;
+        double fallCost = 750.00;
+        double winterCost = 850.00;
+        double totalCost = 0;
+
+        totalCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("The cost for spring was " + springCost);
+        System.out.println("The cost for summer was " + summerCost);
+        System.out.println("The cost for fall was " + fallCost);
+        System.out.println("The cost for winter was " + winterCost);
+        System.out.println("the total yearly cost was " + totalCost);
+    }
+}
